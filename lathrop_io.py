443573acbd5524743139493b8a2db2e3d923c534
Carter Lathrop <clathrop@salesforce.com>
2017-01-11 00:24:29 -0800

    Adds key

--- lathrop_io.py
+++ lathrop_io.py
@@ -4,6 +4,8 @@ from flask import Flask, request, session, g, redirect, url_for, \
 
 from contextlib import closing
 
+app.secret_key = 'mYS3cretKey'
+
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
